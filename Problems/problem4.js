ff6f30825fa0d1b59454f19051ba0c368e291a5c
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-10 15:12:29 +0530

    Problem 4 and Testproblem 4

---
+++ Problems/problem4.js
@@ -0,0 +1,12 @@
+const myModule = require('../Inventory/inventory');
+let val= myModule.inventory
+let years=[];
+module.exports = {
+problem4: function (){
+    for(let i=0; i< val.length; i++)
+    {
+        years.push(val[i].car_year);
+    }
+    return years + '';
+}
+}
---
+++ Problems/test/testproblem4.js
@@ -0,0 +1,3 @@
+const myModule = require('../problem4');
+let v = myModule.problem4;
+console.log(v())
