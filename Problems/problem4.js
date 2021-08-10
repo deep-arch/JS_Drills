d707face0e7749b91653cd9420654b13f9934a12
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-10 15:52:58 +0530

    Problem 4 and Testproblem 4

--- Problems/problem4.js
+++ Problems/problem4.js
@@ -2,7 +2,8 @@ const myModule = require('../Inventory/inventory');
 let val= myModule.inventory
 let years=[];
 module.exports = {
-problem4: function (){
+problem4: function ()
+{
     for(let i=0; i< val.length; i++)
     {
         years.push(val[i].car_year);
