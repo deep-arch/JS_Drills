a5078938bb52a2ab4e6d10f780fc9a4102e898a2
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-10 16:30:04 +0530

    Problem 1 and Testproblem 1

--- Problems/problem1.js
+++ Problems/problem1.js
@@ -1,13 +1,16 @@
 const myModule = require('../Inventory/inventory');
 let val= myModule.inventory
+
+function p1(val)
+{
+    for (var i= 0; i< val.length; i++)
+    {
+        if (val[i].id == 33)
+        {
+            return "Car " + val[i].id + " is a " + val[i].car_year+" "+ val[i].car_make+ " "+val[i].car_model;
+        } 
+    }
+}
 module.exports = {
-problem1: function (){
-for (var i= 0; i< val.length; i++)
-{
-    if (val[i].id == 33)
-{
-    return "Car " + val[i].id + " is a " + val[i].car_year+" "+ val[i].car_make+ " "+val[i].car_model;
-} 
-}
-}
+    problem1: p1(val)
 }
