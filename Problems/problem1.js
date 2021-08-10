192fa65965da69fc7da09f5bf4a6b7058e033b9f
Subhadeep Bandyopadhyay <88320279+deep-arch@users.noreply.github.com>
2021-08-10 16:15:17 +0530

    Problem 1

--- Problems/problem1.js
+++ Problems/problem1.js
@@ -1,13 +1,14 @@
 const myModule = require('../Inventory/inventory');
 let val= myModule.inventory
 module.exports = {
-problem1: function (){
-for (var i= 0; i< val.length; i++)
+problem1: function ()
 {
-    if (val[i].id == 33)
-{
-    return "Car " + val[i].id + " is a " + val[i].car_year+" "+ val[i].car_make+ " "+val[i].car_model;
-} 
-}
+    for (var i= 0; i< val.length; i++)
+    {
+        if (val[i].id == 33)
+        {
+            return "Car " + val[i].id + " is a " + val[i].car_year+" "+ val[i].car_make+ " "+val[i].car_model;
+        } 
+    }
 }
 }
