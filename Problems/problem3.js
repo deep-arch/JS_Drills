59d4e3fbd1240dc2e2201205ea4403a869ea22e6
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 10:26:22 +0530

    Problem3

--- Problems/problem3.js
+++ Problems/problem3.js
@@ -1,12 +1,23 @@
 // ==== Problem #3 ====
 // The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
+
+function lower(str)                              // to convert uppercase to lower case
+{
+    for( let i=0; i<=str.length; i++)
+    {
+        if(str[i]>=65 && str[i]<=90)
+           str[i] = str[i] + 32;
+    }
+    return str;
+}
+
 module.exports = {
 problem3: function(val)
 {
-    for( let i=0; i<val.length;i++)             // using Bubble sort
+    for( let i=0; i<val.length; i++)             // using Bubble sort
         for( let j=0; j<val.length-i-1;j++)
              {
-                if(val[j].car_model.toLowerCase()>val[j+1].car_model.toLowerCase()) 
+                if(lower(val[j].car_model)>lower(val[j+1].car_model)) 
                 {
                     let temp=val[j+1];
                     val[j+1]=val[j]; 
@@ -16,3 +27,4 @@ problem3: function(val)
     return val;
 }
 }
+
