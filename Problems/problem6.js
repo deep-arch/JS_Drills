247d92d9766bf547c568e5511d74151123e23ecf
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 16:41:45 +0530

    Problem6

--- Problems/problem6.js
+++ Problems/problem6.js
@@ -1,7 +1,7 @@
 // ==== Problem #6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 
-let make = []
+let make = [];
 module.exports = {
 problem6 (val) {
     for(let i=0; i< val.length; i++)
