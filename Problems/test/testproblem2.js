8bc402118dcbb937f5b303ef3f6c9b64196fce98
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 16:43:49 +0530

    Testproblems

--- Problems/test/testproblem2.js
+++ Problems/test/testproblem2.js
@@ -1,4 +1,4 @@
 const { inventory } = require('../../Inventory/inventory');
 const { problem2 } = require('../problem2');
-let result = problem2(inventory)
-console.log(result)
+let result = problem2(inventory);
+console.log(result);
