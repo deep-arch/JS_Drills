007994e38b5d4143a77777731e97b33414345f54
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 10:14:44 +0530

    Problem3

--- Problems/problem3.js
+++ Problems/problem3.js
@@ -1,3 +1,5 @@
+// ==== Problem #3 ====
+// The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
 module.exports = {
 problem3: function(val)
 {
