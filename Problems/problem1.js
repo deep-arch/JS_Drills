5367df38f9d06bd8a8624a7ade8f8c80889f4661
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 10:07:16 +0530

    Problem1

--- Problems/problem1.js
+++ Problems/problem1.js
@@ -1,3 +1,5 @@
+// ==== Problem #1 ====
+// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of: 
 module.exports = {
 problem1: function (val)
 {
