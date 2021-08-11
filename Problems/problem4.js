07997f8ff28163d615e97ce6e20e00d55090db76
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-08-11 17:17:10 +0530

    Problem4

--- Problems/problem4.js
+++ Problems/problem4.js
@@ -1,7 +1,7 @@
 // ==== Problem #4 ====
 // The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
 
-let years=[];
+let years = [];
 module.exports = {
 problem4(val)
 { 
@@ -9,6 +9,6 @@ problem4(val)
   {
     years.push(val[i].car_year);
   }
-  return (years);
+  return years;
 }
 }
